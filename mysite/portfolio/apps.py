ea3aac142eebb1bfdabef0a51a91f54ad01a6812
GatMaximoAr <gaticamaxiimo2@gmail.com>
2024-01-14 11:48:50 -0300

    first commit

---
+++ mysite/portfolio/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PortfolioConfig(AppConfig):
+    """Django pre-define class."""
+    
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portfolio'
